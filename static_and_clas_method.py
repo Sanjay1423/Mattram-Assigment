314d69b043569d66f94d8b17ae30ff538301413b
Sanjay1423 <sanjayssp1423@gmail.com>
2022-08-14 09:55:10 +0530

    Mattram Assigment 2

--- static_and_clas_method.py
+++ static_and_clas_method.py
@@ -10,16 +10,20 @@ class Person:
         self.sex = sex
         self.city = city
 
+    # normal method which holds the object name as a first argument as 'self'
     def person_bio(self):
         bio = f'{self.name} is {self.age} years old living in {self.city} and {self.name} is a {self.sex}'
         return bio
 
+    # with class method we can access and modify the class variable
     @classmethod
+    # We can access with the argument of 'cls' which holds the class name
     def change_avg_income(cls, income):
         new_avg_income = sum(income)/len(income)
         cls.avg_income = new_avg_income
         return cls.avg_income
 
+    # Static method doesnt need a class name(cls) or object name(self)
     @staticmethod
     def driving_eligibility(driving_license):
         if driving_license == True:
@@ -30,7 +34,8 @@ class Person:
 person1 = Person('Sanjay', 19, 'male', 'Chennai')
 person2 = Person('Sriram', 20, 'Male', 'Banglore')
 
-# Accessing Class method change_avg_income()
+# Accessing Class method change_avg_income() with class name
 Person.change_avg_income([12000, 10000, 15000, 17000])
 
-print(person1.driving_eligibility(False))
+# Accessing the static method driving_elgiblilty()
+person1.driving_eligibility(False)
